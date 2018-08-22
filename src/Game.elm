011275e57c5f0c5f8a6fa7d04ef02bf99d92dfa0
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-08-22 15:25:51 +0800

    TEMP: comment keyboard code (not in 0.19 yet)

--- src/Game.elm
+++ src/Game.elm
@@ -1,7 +1,8 @@
 module Game exposing (..)
 
-import Keyboard
-import Keyboard.Arrows as Arrows
+-- import Keyboard
+-- import Keyboard.Arrows as Arrows
+
 import Physical.Ball as Ball exposing (Ball)
 import Physical.Block as Block exposing (Block)
 import Physical.Bullet as Bullet exposing (Bullet)
@@ -52,109 +53,115 @@ players { player1, player2, player3, player4 } =
     [ player1, player2, player3, player4 ]
 
 
-update : Time.Posix -> Int -> List Keyboard.Key -> Game -> Game
-update frameTime duration keys ({ player1 } as game) =
-    let
-        thrustArrowsDirection =
-            Arrows.arrowsDirection keys
-
-        thrusting =
-            thrustArrowsDirection /= Arrows.NoDirection
-
-        newDirection =
-            fromArrows thrustArrowsDirection player1.direction
-
-        spaceBarDown =
-            List.member Keyboard.Spacebar keys
-
-        ( newPlayer1, hasShot1 ) =
-            if thrusting then
-                Player.thrustMove duration newDirection player1
-                    |> Player.updateShot frameTime spaceBarDown
-
-            else
-                Player.freefallMove duration newDirection player1
-                    |> Player.updateShot frameTime spaceBarDown
-
-        bullets =
-            case hasShot1 of
-                Player.ShotAfter _ ->
-                    [ Bullet.small newPlayer1.direction newPlayer1.pos ]
-                        |> List.map (Bullet.move duration)
-
-                _ ->
-                    game.bullets
-                        |> List.map (Bullet.move duration)
-    in
-    { game | player1 = newPlayer1, bullets = bullets }
+update : Time.Posix -> Int -> Game -> Game
+update frameTime duration ({ player1 } as game) =
+    game
 
 
-fromArrows : Arrows.Direction -> Float -> Float
-fromArrows arrowsDirection previousDirection =
-    case arrowsDirection of
-        Arrows.North ->
-            north
 
-        Arrows.NorthEast ->
-            northEast
-
-        Arrows.East ->
-            east
-
-        Arrows.SouthEast ->
-            southEast
-
-        Arrows.South ->
-            south
-
-        Arrows.SouthWest ->
-            southWest
-
-        Arrows.West ->
-            west
-
-        Arrows.NorthWest ->
-            northWest
-
-        _ ->
-            previousDirection
-
-
-north : Float
-north =
-    -pi / 2
-
-
-northEast : Float
-northEast =
-    -pi / 4
-
-
-east : Float
-east =
-    0
-
-
-southEast : Float
-southEast =
-    pi / 4
-
-
-south : Float
-south =
-    pi / 2
-
-
-southWest : Float
-southWest =
-    3 * pi / 4
-
-
-west : Float
-west =
-    pi
-
-
-northWest : Float
-northWest =
-    -3 * pi / 4
+-- update : Time.Posix -> Int -> List Keyboard.Key -> Game -> Game
+-- update frameTime duration keys ({ player1 } as game) =
+--     let
+--         thrustArrowsDirection =
+--             Arrows.arrowsDirection keys
+--
+--         thrusting =
+--             thrustArrowsDirection /= Arrows.NoDirection
+--
+--         newDirection =
+--             fromArrows thrustArrowsDirection player1.direction
+--
+--         spaceBarDown =
+--             List.member Keyboard.Spacebar keys
+--
+--         ( newPlayer1, hasShot1 ) =
+--             if thrusting then
+--                 Player.thrustMove duration newDirection player1
+--                     |> Player.updateShot frameTime spaceBarDown
+--
+--             else
+--                 Player.freefallMove duration newDirection player1
+--                     |> Player.updateShot frameTime spaceBarDown
+--
+--         bullets =
+--             case hasShot1 of
+--                 Player.ShotAfter _ ->
+--                     [ Bullet.small newPlayer1.direction newPlayer1.pos ]
+--                         |> List.map (Bullet.move duration)
+--
+--                 _ ->
+--                     game.bullets
+--                         |> List.map (Bullet.move duration)
+--     in
+--     { game | player1 = newPlayer1, bullets = bullets }
+--
+--
+-- fromArrows : Arrows.Direction -> Float -> Float
+-- fromArrows arrowsDirection previousDirection =
+--     case arrowsDirection of
+--         Arrows.North ->
+--             north
+--
+--         Arrows.NorthEast ->
+--             northEast
+--
+--         Arrows.East ->
+--             east
+--
+--         Arrows.SouthEast ->
+--             southEast
+--
+--         Arrows.South ->
+--             south
+--
+--         Arrows.SouthWest ->
+--             southWest
+--
+--         Arrows.West ->
+--             west
+--
+--         Arrows.NorthWest ->
+--             northWest
+--
+--         _ ->
+--             previousDirection
+--
+--
+-- north : Float
+-- north =
+--     -pi / 2
+--
+--
+-- northEast : Float
+-- northEast =
+--     -pi / 4
+--
+--
+-- east : Float
+-- east =
+--     0
+--
+--
+-- southEast : Float
+-- southEast =
+--     pi / 4
+--
+--
+-- south : Float
+-- south =
+--     pi / 2
+--
+--
+-- southWest : Float
+-- southWest =
+--     3 * pi / 4
+--
+--
+-- west : Float
+-- west =
+--     pi
+--
+--
+-- northWest : Float
+-- northWest =
+--     -3 * pi / 4
--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (..)
 
+-- import Keyboard
+
 import Browser
 import Browser.Events
+import Collision
 import Game exposing (Game)
 import Html exposing (Html)
 import Html.Attributes
-import Keyboard
 import Physical.Ball
 import Physical.Block
 import Physical.Bullet
@@ -45,14 +47,18 @@ type alias Model =
     , frameSize : Size
     , frameTime : Time.Posix
     , game : Game
-    , pressedKeys : List Keyboard.Key
+
+    -- , pressedKeys : List Keyboard.Key
     }
 
 
 type Msg
     = NewFrame Time.Posix
     | Resizes Size
-    | KeyMsg Keyboard.Msg
+
+
+
+-- | KeyMsg Keyboard.Msg
 
 
 init : Flags -> ( Model, Cmd Msg )
@@ -61,7 +67,8 @@ init { time, size } =
       , frameSize = size
       , frameTime = Time.millisToPosix time
       , game = Game.init (Time.millisToPosix time)
-      , pressedKeys = []
+
+      -- , pressedKeys = []
       }
     , Cmd.none
     )
@@ -72,7 +79,8 @@ subscriptions _ =
     Sub.batch
         [ Browser.Events.onAnimationFrame NewFrame
         , Ports.resizes Resizes
-        , Sub.map KeyMsg Keyboard.subscriptions
+
+        -- , Sub.map KeyMsg Keyboard.subscriptions
         ]
 
 
@@ -90,28 +98,29 @@ update msg model =
             ( { model
                 | frameSize = model.size
                 , frameTime = time
-                , game = Game.update model.frameTime duration model.pressedKeys model.game
+
+                -- , game = Game.update model.frameTime duration model.pressedKeys model.game
+                , game = Game.update model.frameTime duration model.game
               }
             , Cmd.none
             )
 
-        KeyMsg keyMsg ->
-            let
-                parser =
-                    Keyboard.oneOf
-                        [ Keyboard.whitespaceKey
-                        , Keyboard.navigationKey
-                        ]
-
-                pressedKeys =
-                    Keyboard.updateWithParser parser keyMsg model.pressedKeys
-            in
-            ( { model | pressedKeys = pressedKeys }
-            , Cmd.none
-            )
 
 
-
+-- KeyMsg keyMsg ->
+--     let
+--         parser =
+--             Keyboard.oneOf
+--                 [ Keyboard.whitespaceKey
+--                 , Keyboard.navigationKey
+--                 ]
+--
+--         pressedKeys =
+--             Keyboard.updateWithParser parser keyMsg model.pressedKeys
+--     in
+--     ( { model | pressedKeys = pressedKeys }
+--     , Cmd.none
+--     )
 -- VIEW ####################################################
 
 
